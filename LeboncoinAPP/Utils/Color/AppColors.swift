c6a6e4b3a6af1d340b1062a6a6e33c63ca69c949
Daniel Martin Jimenez <danmarjim@gmail.com>
2025-04-21 12:35:10 +0200

    feat: LBCOIN - 005 - Add app icon and custom colors

---
+++ LeboncoinAPP/Utils/Color/AppColors.swift
@@ -0,0 +1,6 @@
+import SwiftUI
+
+struct AppColors {
+  static let primary = #colorLiteral(red: 0.8156862745, green: 0.3725490196, blue: 0.1215686275, alpha: 1)
+  static let secondary = #colorLiteral(red: 0.9333333333, green: 0.9411764706, blue: 0.9333333333, alpha: 1)
+}
