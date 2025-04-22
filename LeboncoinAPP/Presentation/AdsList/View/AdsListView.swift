0458cce1c310d4dccce214f0e8d13d1fec2d3ce3
Daniel Martin Jimenez <danmarjim@gmail.com>
2025-04-22 11:06:11 +0200

    feat: LBCOIN - 008 - Custom handle errors

--- LeboncoinAPP/Presentation/AdsList/View/AdsListView.swift
+++ LeboncoinAPP/Presentation/AdsList/View/AdsListView.swift
@@ -8,8 +8,17 @@ struct AdsListView: View {
       VStack {
         if viewModel.isLoading {
           ProgressView()
-        } else if let _ = viewModel.error {
-          
+        } else if let error = viewModel.error {
+          VStack {
+            Text("Something went wrong:")
+              .font(.headline)
+            Text(error.localizedDescription)
+              .font(.subheadline)
+            Button("Retry") {
+              viewModel.retry()
+            }
+            .padding(.top)
+          }
         } else {
           CategoryListView(selectedCategory: $viewModel.selectedCategory, categories: viewModel.categories)
             .padding([.top, .bottom], Spacing.m)
